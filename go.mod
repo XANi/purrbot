7bd071504614ce550eb2ccfdc71cd0046c7109ac
Mariusz Gronczewski <xani666@gmail.com>
2019-11-20 05:49:08 +0100

    tui

--- go.mod
+++ go.mod
@@ -1,9 +1,12 @@
 module github.com/XANi/purrbot
 
 require (
-	github.com/XANI/go-yamlcfg v0.0.1
 	github.com/XANi/go-gitcli v0.0.0-20170104224521-b063a6525a92
 	github.com/XANi/go-yamlcfg v0.0.1
+	github.com/kr/pretty v0.1.0 // indirect
 	github.com/op/go-logging v0.0.0-20160315200505-970db520ece7
-	gopkg.in/yaml.v1 v1.0.0-20140924161607-9f9df34309c0
+	github.com/rivo/tview v0.0.0-20191018125527-685bf6da76c2
+	github.com/smartystreets/goconvey v1.6.4 // indirect
+	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
+	gopkg.in/yaml.v1 v1.0.0-20140924161607-9f9df34309c0 // indirect
 )
--- purrbot.go
+++ purrbot.go
@@ -3,12 +3,13 @@ package main
 import (
 	"fmt"
 	"github.com/op/go-logging"
-	"os"
+
 	//	"strings"
 	"github.com/XANi/go-yamlcfg"
 	"github.com/XANi/purrbot/config"
 	"github.com/XANi/purrbot/plugins/git"
 	"github.com/XANi/purrbot/utils"
+	"github.com/rivo/tview"
 )
 
 var version string
@@ -16,10 +17,6 @@ var log = logging.MustGetLogger("main")
 var stdout_log_format = logging.MustStringFormatter("%{color:bold}%{time:2006-01-02T15:04:05.0000Z-07:00}%{color:reset}%{color} [%{level:.1s}] %{color:reset}%{shortpkg}[%{longfunc}] %{message}")
 
 func main() {
-	stderrBackend := logging.NewLogBackend(os.Stderr, "", 0)
-	stderrFormatter := logging.NewBackendFormatter(stderrBackend, stdout_log_format)
-	logging.SetBackend(stderrFormatter)
-	logging.SetFormatter(stdout_log_format)
 
 	// TODO term detection ?
 	utils.UpdateXtermTitle(fmt.Sprintf("Purrbot v%s", version))
@@ -34,12 +31,40 @@ func main() {
 		log.Panicf("Config error: %s", err)
 	}
 
+	app := tview.NewApplication()
+	textView := tview.NewTextView().
+		SetDynamicColors(true).
+		SetScrollable(false).
+		SetChangedFunc(func() {
+			app.Draw()
+		})
+	textView.SetBorder(true).SetTitle("log")
+	w := tview.ANSIWriter(textView)
+	stderrBackend := logging.NewLogBackend(w, "", 0)
+	stderrFormatter := logging.NewBackendFormatter(stderrBackend, stdout_log_format)
+	logging.SetBackend(stderrFormatter)
+	logging.SetFormatter(stdout_log_format)
+
 	log.Noticef("Config: %+v", cfg)
 	if pluginCfg, ok := cfg.Plugins["git"]; ok {
 		gp, err := git.New(pluginCfg)
 		if err != nil {
 			log.Errorf("can't start git plugin: %s", err)
 		}
-		gp.Run()
+		go gp.Run()
+		_ = gp
 	}
+
+	flex := tview.NewFlex().SetDirection(tview.FlexRow).
+		AddItem(tview.NewBox().SetBorder(true).SetTitle("top"), 0, 1, false).
+		AddItem(tview.NewFlex().SetDirection(tview.FlexColumn).
+			AddItem(tview.NewBox().SetBorder(true).SetTitle("left"), 0, 1, false).
+			//AddItem(tview.NewBox().SetBorder(true).SetTitle("Middle (3 x height of Top)"), 0, 3, false).
+			AddItem(tview.NewBox().SetBorder(true).SetTitle("right"), 0, 2, false), 0, 2, false).
+		AddItem(textView, 10, 1, false)
+
+	if err := app.SetRoot(flex, true).Run(); err != nil {
+		panic(err)
+	}
+
 }
